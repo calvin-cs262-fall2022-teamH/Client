4c26d8d24b79688b49d0fded950348923972c49c
Logan Humphrey <lhumphrey3511@gmail.com>
2022-10-07 00:37:39 -0400

    Prettying up each day + adding global variables

--- App.js
+++ App.js
@@ -5,6 +5,9 @@ import ReactNativeZoomableView from '@dudigital/react-native-zoomable-view/src/R
 /*  A space to declare global variables */
 let primary = "#F38C00";  // def primary color (orange)
 let secondary = "#0167AB"; // def secondary color (blue)
+let schedHeight = 600;    //def height per day view
+let heightPiece = (schedHeight - 100) / 16; //height peice per hour
+let schedWidth = 350;     //def width per day view
 
 
 /* function: hLine  -  draws horizontal lines
@@ -22,23 +25,28 @@ function hLine(length, x, y, color) {
   />;
 };
 
+/* function: renderGrid  -  time & lines placed
+  @params: item
+*/
 function renderGrid(item) {
   const timeView = [];
   const lineView = [];
-  const times = ["  8 AM", "  9 AM", "10 AM", "11 AM", "12 AM", "  1 PM", "  2 PM", "  3 PM", "  4 PM", "  5 PM", "  6 PM", "  7 PM", "  8 PM", "  9 PM", "10 PM", "11 PM", "12 PM"]
+  const times = ["  8 AM", "  9 AM", "10 AM", "11 AM", "12 AM", "  1 PM", "  2 PM", "  3 PM", "  4 PM", "  5 PM", "  6 PM", "  7 PM", "  8 PM", "  9 PM", "10 PM", "11 PM", "12 PM",]
   for (let i = 0; i <= 16; i += 1) {
-    timeView.push(<Text style={{ position: 'absolute', color: "white", marginTop: i * 20 + 30 }}>{times[i]}</Text>);     //  writes times
-    lineView.push(<View style={{ position: 'absolute' }}>{hLine(280, 50, i * 20 + 40, "white", 1)}</View>)               //  draws horiz lines
+    // timeView.push(<Text style={{ position: 'absolute', color: "white", marginTop: i * 20 + 30 }}>{times[i]}</Text>);     //  writes times
+    // lineView.push(<View style={{ position: 'absolute' }}>{hLine(280, 50, i * 20 + 40, "white", 1)}</View>)               //  draws horiz lines
+    timeView.push(<Text style={{ position: 'absolute', color: "white", marginTop: i * heightPiece + 50 }}>{times[i]}</Text>);     //  writes times
+    lineView.push(<View style={{ position: 'absolute' }}>{hLine(280, 50, i * heightPiece + 60, "white", 1)}</View>)               //  draws horiz lines
   }
   return <View>
-    <Text style={{ textAlign: "center" }}>{item}</Text>
+    <Text style={{ textAlign: "center", fontSize: 24, color: "white" }}>{item}</Text>
     {timeView}{lineView}
   </View>;
 };
 
 const Listitem = ({ item }) => {
   return (
-    <View style={{ height: 600, width: 350, backgroundColor: primary, borderRadius: 5 }}>
+    <View style={{ height: schedHeight, width: schedWidth, backgroundColor: primary, borderRadius: 5 }}>
       {renderGrid(item)}
     </View>
   );
@@ -51,9 +59,12 @@ export default function App() {
         marginTop: 120,   //creates room for "toolbar"
         marginLeft: 10,
         marginRight: 10,
+        title: {
+          fontSize: 32,
+        },
       }}
       horizontal
-      data={["Mon", "Tues", "Wed", "Thurs", "Fri"]}
+      data={["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]}
       renderItem={({ item }) => <Listitem item={item} />}
       showsHorizontalScrollIndicator={true}
       showsVerticalScrollIndicator={true}
