97df63d7a0d17f37ecf9d8de0f4d664e7ddcc5a0
Logan Humphrey <lhumphrey3511@gmail.com>
2022-10-13 23:46:39 -0400

    homeScreen changes: icons and non-scroll toolbar

--- App.js
+++ App.js
@@ -1,16 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { Image, ScrollView, StyleSheet, Switch, Text, View, FlatList, TouchableOpacity } from 'react-native';
+import { Image, ScrollView, StyleSheet, Switch, Text, View, FlatList, TouchableOpacity, Dimensions } from 'react-native';
 import ReactNativeZoomableView from '@dudigital/react-native-zoomable-view/src/ReactNativeZoomableView';
 
 /*  A space to declare global variables */
-let primary = "#F3F3F3";    //  default primary color     (orange)
-let secondary = "#0167AB";  //  default secondary color   (blue)
-let bgColor = "white";    //  default background color  (white)
-let textColor = "black";  //  default textcolor         (black)
-let scrollSchedHeight = window.innerHeight;  // def scrollView height of schedule
-let schedHeight = 1000; //def height per day (within the scroll view) nested within scrollSchedHeight
-let heightPiece = (schedHeight - 100) / 16; //height peice per hour
-let schedWidth = 350;     //def width per day view
+let primary = "#F38C00";      //  default primary color     (orange)
+let secondary = "#0167AB";    //  default secondary color   (blue)
+let bgColor = "white";        //  default background color  (white in LM) (black in DM)
+let textColor = "black";      //  default textcolor         (black in LM) (white in DM)
+const windowWidth = Dimensions.get('window').width;         //full height of screen
+const windowHeight = Dimensions.get('window').height;       //full width of screen
+let heightPiece = (windowHeight) / 12;                      //height peice per hour
 
 
 /* function: hLine    to draw horizontal lines
@@ -31,6 +30,19 @@ function hLine(length, x, y, color) {
 /* function: goHome    workAround logo clicked -> homeScreen */
 function goHome() { }
 
+/* function: goSettings    settings icon clicked -> settings page */
+function goSettings() { }
+
+/* function: goShare      share icon clicked -> share popup */
+function goShare() { }
+
+/* function: goProfile      profile icon clicked -> profiles page */
+function goProfile() { }
+
+/* function: goNotification      notification bell icon clicked -> notification popup */
+function goNotification() { }
+
+
 /* function: renderGrid  -  times & lines placed
   @params: item
 */
@@ -39,68 +51,128 @@ function renderGrid(item) {
   const lineView = [];
   const times = ["  8 am", "  9 am", "10 am", "11 am", "12 am", "  1 pm", "  2 pm", "  3 pm", "  4 pm", "  5 pm", "  6 pm", "  7 pm", "  8 pm", "  9 pm", "10 pm", "11 pm", "12 am",]
   for (let i = 0; i <= 16; i += 1) {
-    timeView.push(<Text style={{ position: 'absolute', color: textColor, marginTop: i * heightPiece + 50 }}>{times[i]}</Text>);     //  write times
-    lineView.push(<View style={{ position: 'absolute' }}>{hLine(280, 50, i * heightPiece + 60, textColor, 1)}</View>)               //  draw horiz lines
+    timeView.push(<Text style={{ position: 'absolute', color: textColor, marginTop: i * heightPiece + 60 }}>{times[i]}</Text>);     //  write times
+    lineView.push(<View style={{ position: 'absolute' }}>{hLine(280, 50, i * heightPiece + 70, textColor, 1)}</View>)               //  draw horiz lines
   }
+  {/* Style class view */ }
   return <View>
-    {/* Style day text */}
+    {/* Style day-of-week text */}
     <Text style={{ textAlign: "center", fontSize: 24, color: textColor }}>{item}</Text>
     {/* Display both arrays */}
     {timeView}{lineView}
   </View>;
 };
 
+/* function: Listitem  -  creates each schedule widget
+  @params: item
+*/
 const Listitem = ({ item }) => {
   return (
-    <View style={{ height: schedHeight, width: schedWidth, backgroundColor: primary, borderRadius: 5 }}>
-
+    <View style={{
+      height: 1.5 * windowHeight,
+      width: windowWidth - 20,
+      backgroundColor: "#EFEFEF",
+      borderRadius: 5,
+      marginRight: 20
+    }}>
       {renderGrid(item)}
     </View>
   );
 };
 
+
 export default function App() {
   return (
-    <FlatList
-      style={{
-        marginTop: 25,
-        marginBottom: 70,
-        marginHorizontal: 10,
-      }}
-      horizontal
-      data={["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]}
-      renderItem={({ item }) => <View style={{ height: scrollSchedHeight }}>
-        <View style={{ backgroundColor: "white", height: 100, }}>
-          <Text>Header</Text>
-          <TouchableOpacity
-            style={{ backgroundColor: "blue", position: "relative", x: 0 }}
-            onPress={goHome}>
-            <Image style={{
-              height: 80,
-              width: 150,
-              resizeMode: "cover",
-              position: "absolute",
-              marginLeft: 200,
+    <View>
 
-            }} source={require('./workAround.png')} />
+      {/* Header Toolbar (with profile, notification bell, and logo) */}
+      <View style={{ backgroundColor: "white", height: .14 * windowHeight }}>
 
-          </TouchableOpacity>
-        </View>
+        {/* Profile Icon (top left)*/}
+        <TouchableOpacity onPress={goProfile}>
+          <Image style={{
+            height: 35,
+            width: 35,
+            position: "absolute",
+            top: 50,
+            left: 20
+          }} source={require('./Icons/profile.png')} />
+
+        </TouchableOpacity>
+
+        {/* Profile Icon (top left)*/}
+        <TouchableOpacity onPress={goNotification}>
+          <Image style={{
+            height: 35,
+            width: 35,
+            position: "absolute",
+            top: 50,
+            left: 70
+          }} source={require('./Icons/notification.png')} />
+        </TouchableOpacity>
 
 
-        <ScrollView>
-          <Listitem item={item} />
-        </ScrollView>
+        {/* workAround logo / home button */}
+        <TouchableOpacity
+          style={{ position: "absolute" }} onPress={goHome}>
+          <Image style={{
+            height: 80,
+            width: 150,
+            position: "absolute",
+            left: windowWidth - 170,
+            top: 25,
+          }} source={require('./Icons/workAround.png')} />
+        </TouchableOpacity>
+      </View>
 
-        <View class="toolbar" id="#bottom-toolbar">
-          <Text>Footer</Text>
-        </View>
-      </View >}
+      {/* Schedule on homeScreen */}
+      <FlatList
+        style={{
+          borderVerticalColor: "black",
+          borderTopWidth: 3,
+          borderBottomWidth: 3,
+          marginVerital: 5,
+          padding: 10,
+        }}
+        horizontal
+        data={["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]}
+        renderItem={({ item }) => <View style={{ height: .75 * windowHeight }}>
 
-      showsHorizontalScrollIndicator={true}
-      showsVerticalScrollIndicator={true}
-      ItemSeparatorComponent={() => { return (<View style={{ width: 10 }} />); }} //the variable "width" affects space between elements in the list
-    />
+          <ScrollView>
+            <Listitem item={item} />
+          </ScrollView>
+        </View >}
+
+        showsHorizontalScrollIndicator={true}
+        showsVerticalScrollIndicator={true}
+      />
+
+      {/* Footer Toolbar (with settings and share) */}
+      <View style={{ backgroundColor: "white", height: .1 * windowHeight, width: windowWidth, position: "absolute", top: .95 * windowHeight }}>
+
+        {/* Settings Icon (bottom left)*/}
+        <TouchableOpacity onPress={goSettings}>
+          <Image style={{
+            height: 45,
+            width: 45,
+            position: "absolute",
+            right: windowWidth - 70,
+            top: -3,
+          }} source={require('./Icons/settings.png')} />
+        </TouchableOpacity>
+
+        {/* Share Icon (bottom right)*/}
+        <TouchableOpacity onPress={goShare}>
+          <Image style={{
+            height: 35,
+            width: 35,
+            position: "absolute",
+            left: windowWidth - 70,
+          }} source={require('./Icons/share.png')} />
+        </TouchableOpacity>
+
+      </View>
+    </View>
   );
 }
 
