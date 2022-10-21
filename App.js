1377723f3f0947a18f893281bdc670cdc4ffd7ac
Cabroek <113718518+Cabroek@users.noreply.github.com>
2022-10-20 22:46:56 -0400

    Add files via upload

--- App.js
+++ App.js
@@ -1,16 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { ScrollView, StyleSheet, Switch, Text, View, FlatList } from 'react-native';
+import { Image, ScrollView, StyleSheet, Switch, Text, View, FlatList, TouchableOpacity, Dimensions } from 'react-native';
 import ReactNativeZoomableView from '@dudigital/react-native-zoomable-view/src/ReactNativeZoomableView';
 
 /*  A space to declare global variables */
-let primary = "#F38C00";  // def primary color (orange)
-let secondary = "#0167AB"; // def secondary color (blue)
-let schedHeight = 600;    //def height per day view
-let heightPiece = (schedHeight - 100) / 16; //height peice per hour
-let schedWidth = 350;     //def width per day view
+let primary = "#F38C00";      //  default primary color     (orange)
+let secondary = "#0167AB";    //  default secondary color   (blue)
+let bgColor = "bgColor";        //  default background color  (white in LM) (black in DM)
+let textColor = "black";      //  default textcolor         (black in LM) (white in DM)
+const windowWidth = Dimensions.get('window').width;         //full height of screen
+const windowHeight = Dimensions.get('window').height;       //full width of screen
+let heightPiece = (windowHeight) / 12;                      //height peice per hour
 
 
-/* function: hLine  -  draws horizontal lines
+/* function: hLine    to draw horizontal lines
   @params: length, (x, y) displacement, color, thickness
 */
 function hLine(length, x, y, color) {
@@ -25,60 +27,171 @@ function hLine(length, x, y, color) {
   />;
 };
 
-/* function: renderGrid  -  time & lines placed
+/* function: goHome    workAround logo clicked -> homeScreen */
+function goHome() { }
+
+/* function: goSettings    settings icon clicked -> settings page */
+function goSettings() { }
+
+/* function: goShare      share icon clicked -> share popup */
+function goShare() { }
+
+/* function: goProfile      profile icon clicked -> profiles page */
+function goProfile() { }
+
+/* function: goNotification      notification bell icon clicked -> notification popup */
+function goNotification() { }
+
+/* function: goAdd      add icon clicked -> add event page/popup */
+function goAdd() { }
+
+
+/* function: renderGrid  -  times & lines placed
   @params: item
 */
 function renderGrid(item) {
   const timeView = [];
   const lineView = [];
-  const times = ["  8 AM", "  9 AM", "10 AM", "11 AM", "12 AM", "  1 PM", "  2 PM", "  3 PM", "  4 PM", "  5 PM", "  6 PM", "  7 PM", "  8 PM", "  9 PM", "10 PM", "11 PM", "12 PM",]
+  const times = ["  8 am", "  9 am", "10 am", "11 am", "12 am", "  1 pm", "  2 pm", "  3 pm", "  4 pm", "  5 pm", "  6 pm", "  7 pm", "  8 pm", "  9 pm", "10 pm", "11 pm", "12 am",]
   for (let i = 0; i <= 16; i += 1) {
-    // timeView.push(<Text style={{ position: 'absolute', color: "white", marginTop: i * 20 + 30 }}>{times[i]}</Text>);     //  writes times
-    // lineView.push(<View style={{ position: 'absolute' }}>{hLine(280, 50, i * 20 + 40, "white", 1)}</View>)               //  draws horiz lines
-    timeView.push(<Text style={{ position: 'absolute', color: "white", marginTop: i * 2 * heightPiece + 50 }}>{times[i]}</Text>);     //  writes times
-    lineView.push(<View style={{ position: 'absolute' }}>{hLine(280, 50, i * 2 * heightPiece + 60, "white", 1)}</View>)               //  draws horiz lines
+    timeView.push(<Text style={{ position: 'absolute', color: textColor, marginTop: i * heightPiece + 60 }}>{times[i]}</Text>);     //  write times
+    lineView.push(<View style={{ position: 'absolute' }}>{hLine(280, 50, i * heightPiece + 70, textColor, 1)}</View>)               //  draw horiz lines
   }
+  {/* Style class view */ }
   return <View>
-    <Text style={{ textAlign: "center", fontSize: 24, color: "white" }}>{item}</Text>
+    {/* Style day-of-week text */}
+    <Text style={{ textAlign: "center", fontSize: 24, color: textColor }}>{item}</Text>
+    {/* Display both arrays */}
     {timeView}{lineView}
   </View>;
 };
 
+/* function: Listitem  -  creates each schedule widget
+  @params: item
+*/
 const Listitem = ({ item }) => {
   return (
-    <View style={{ height: window.innerHeight, width: schedWidth, backgroundColor: primary, borderRadius: 5 }}>
+    <View style={{
+      height: 1.5 * windowHeight,
+      width: windowWidth - 20,
+      // backgroundColor: "#EFEFEF",
+      borderRadius: 5,
+      marginRight: 20
+    }}>
       {renderGrid(item)}
     </View>
   );
 };
 
+
 export default function App() {
   return (
-    <FlatList
-      style={{
-        marginTop: 120,   //creates room for "toolbar"
-        marginLeft: 10,
-        marginRight: 10,
-        title: {
-          fontSize: 32,
-        },
-      }}
-      horizontal
-      data={["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]}
-      renderItem={({ item }) => <Listitem item={item} />}
-      showsHorizontalScrollIndicator={true}
-      showsVerticalScrollIndicator={true}
-      ItemSeparatorComponent={() => { return (<View style={{ width: 10 }} />); }} //the variable "width" affects space between elements in the list
-    />
+    <View>
+
+      {/* Header Toolbar (with profile, notification bell, and logo) */}
+      <View style={{ backgroundColor: "bgColor", height: .14 * windowHeight }}>
+
+        {/* Profile Icon (top left)*/}
+        <TouchableOpacity onPress={goProfile}>
+          <Image style={{
+            height: 35,
+            width: 35,
+            position: "absolute",
+            top: 50,
+            left: 20
+          }} source={require('./Icons/profile.png')} />
+        </TouchableOpacity>
+
+        {/* Notifcation Icon (top left)*/}
+        <TouchableOpacity onPress={goNotification}>
+          <Image style={{
+            height: 35,
+            width: 35,
+            position: "absolute",
+            top: 50,
+            left: 70
+          }} source={require('./Icons/notification.png')} />
+        </TouchableOpacity>
+
+
+        {/* workAround logo / home button */}
+        <TouchableOpacity
+          style={{ position: "absolute" }} onPress={goHome}>
+          <Image style={{
+            height: 80,
+            width: 150,
+            position: "absolute",
+            left: windowWidth - 170,
+            top: 25,
+          }} source={require('./Icons/workAround.png')} />
+        </TouchableOpacity>
+      </View>
+
+      {/* Schedule on homeScreen */}
+      <FlatList
+        style={{
+          borderVerticalColor: textColor,
+          borderTopWidth: 3,
+          borderBottomWidth: 3,
+          marginVerital: 5,
+          padding: 10,
+        }}
+        horizontal
+        data={["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]}
+        renderItem={({ item }) => <View style={{ height: .75 * windowHeight }}>
+
+          {/* Settings Icon (bottom left)*/}
+          <TouchableOpacity onPress={goAdd}>
+            <Image style={{
+              height: 70,
+              width: 70,
+              position: "absolute",
+              top: .65 * windowHeight,
+              left: .78 * windowWidth,
+              backgroundColor: primary,
+              borderWidth: 6,
+              borderColor: "bgColor",
+              borderRadius: 100,
+
+            }} source={require('./Icons/add.png')} />
+          </TouchableOpacity>
+
+          <ScrollView>
+            <Listitem item={item} />
+          </ScrollView>
+        </View >}
+
+        showsHorizontalScrollIndicator={true}
+        showsVerticalScrollIndicator={true}
+      />
+
+      {/* Footer Toolbar (with settings and share) */}
+      <View style={{ backgroundColor: "bgColor", height: .1 * windowHeight, width: windowWidth, position: "absolute", top: .95 * windowHeight }}>
+
+        {/* Settings Icon (bottom left)*/}
+        <TouchableOpacity onPress={goSettings}>
+          <Image style={{
+            height: 45,
+            width: 45,
+            position: "absolute",
+            right: windowWidth - 70,
+            top: -3,
+          }} source={require('./Icons/settings.png')} />
+        </TouchableOpacity>
+
+        {/* Share Icon (bottom right)*/}
+        <TouchableOpacity onPress={goShare}>
+          <Image style={{
+            height: 35,
+            width: 35,
+            position: "absolute",
+            left: windowWidth - 70,
+          }} source={require('./Icons/share.png')} />
+        </TouchableOpacity>
+
+      </View>
+    </View>
   );
 }
 
-const styles = StyleSheet.create({
-  zoomWrapper: {
-    flex: 1,
-    backgroundColor: "grey",
-    overflow: 'hidden',
-    height: 2000,
-    length: 1000,
-  },
-});
+const styles = StyleSheet.create({});
---
+++ Components/event.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+
+import { StatusBar } from 'expo-status-bar';
+import { ScrollView, StyleSheet, Switch, Text, View, FlatList, TouchableOpacity } from 'react-native';
+import ReactNativeZoomableView from '@dudigital/react-native-zoomable-view/src/ReactNativeZoomableView';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { globalStyles } from '../styles/global'
+
+export default function Event({ navigation }) {
+    const [details, setDetails] = useState([
+        {
+            title: "CS-108", startTime: "10:00", endTime: "11:30",
+            location: "223 Science Building"
+        },
+        {
+            title: "Eng-220", startTime: "12:00", endTime: "1:00",
+            location: "254 Fine Arts Center"
+        },
+    ]);
+
+    return (
+        <View style={globalStyles.eventContainer}>
+            <FlatList data={details} renderItem={({ item }) => (
+                <TouchableOpacity
+                    onPress={() => navigation.navigate('Edit')} >
+
+                    <Text style={globalStyles.Text}>
+                        {item.title}
+                    </Text>
+                    <Text style={globalStyles.Text}>
+                        {item.startTime}
+                    </Text>
+                    <Text style={globalStyles.Text}>
+                        {item.endTime}
+                    </Text>
+                    <Text style={globalStyles.Text}>
+                        {item.location}
+                    </Text>
+                </TouchableOpacity>
+            )} />
+        </View>
+
+    )
+}
+
+
