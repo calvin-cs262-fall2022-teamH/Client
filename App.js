dd2ee9f6032e22d733c1189b065396fb1ab3b88f
Cabroek <113718518+Cabroek@users.noreply.github.com>
2022-12-10 12:15:39 -0500

    Updating notification alerts

--- App.js
+++ App.js
@@ -17,7 +17,7 @@
   - Logan Humphrey
 */
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   Animated,
   Image,
@@ -25,6 +25,7 @@ import {
   View,
   TouchableOpacity,
   Dimensions,
+  DrawerLayoutAndroidBase,
 } from "react-native";
 import EventCalendar from "react-native-events-calendar";
 import { NavigationContainer } from "@react-navigation/native";
@@ -39,8 +40,10 @@ import SettingsScreen from "./screens/settings";
 import ProfileScreen from "./screens/profile";
 import AddScreen from "./screens/add";
 import Options from "./screens/options";
-import DeleteScreen from "./screens/delete";
+import Delete from "./screens/delete";
 import EditScreen from "./screens/edit";
+import NotificationsScreen from "./screens/notifications";
+import DeleteScreen from './screens/delete';
 
 
 /*  A space to declare global variables */
@@ -100,21 +103,102 @@ function Header() {
       <View style={{ flex: 1, flexDirection: "row", flexWrap: "wrap" }}>
 
         {/* workAround logo / home button */}
-        <TouchableOpacity
-          style={{ position: "absolute" }} onPress={goHome}>
+        <View >
           <Image style={{
             height: 85,
             width: 160,
-            left: 40,
+            left: 100,
             top: -50,
           }} source={require('./Icons/workAround.png')} />
-        </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
 };
 
 
+// function repeatClasses(classes) {
+//   var finalClasses = [];
+//   const regex = /(.*)-(.*)-(.*) (.*)/; //regex to parse startTime and endTime of elements of each course
+//   for (var i = 0; i < classes.length; i++) {
+//     var values = Object.values(classes[i]); // [name, location, start time, end time]
+//     var startTimeData = [...values[2].matchAll(regex)][0]; // [original string, year, month, date, time]
+//     var endTimeData = [...values[3].matchAll(regex)][0]; // [original string, year, month, date, time]
+//     var startDay = parseInt(startTimeData[2]) * 31 + parseInt(startTimeData[3]);
+//     for (var x = startDay; x <= startDay + 4; x++) { //change middle number to change num days to repeat for+1(note that months are set to 31 days as a standard)
+//       var day = ((x % 31) + 31) % 31; //calculates day
+//       var month = Math.floor(x / 31) % 12; //calculates month
+//       var year = Math.floor(x / 372) + parseInt(startTimeData[1]); //calculates if the year needs added to
+//       var startTime = year.toString() + '-' + month.toString() + '-' + day.toString() + ' ' + startTimeData[4];
+//       var endTime = year.toString() + '-' + month.toString() + '-' + day.toString() + ' ' + endTimeData[4];
+//       finalClasses.push({
+//         title: values[0],
+//         summary: values[1],
+//         start: startTime.toString(),
+//         end: endTime.toString(),
+//       });
+//     }
+//   }
+//   return (finalClasses);
+// }
+
+//takes in date obj and startTime and returns string in format YYYY-MM-DD TT:TT:TT 
+//where date portion is drom date and time is from startTime
+function formatedDate(date, startTime){
+  var month = date.getMonth()+1; //because the month is given as an index
+  //for some reason, event calendar needs all single digit values to have a 0 infront, hence all the offsetter values
+  var zeroOffsetter = '';
+  var dayOffsetter = '';
+  var monthOffsetter = '';
+  var monthStr = month.toString();
+  var day = date.getDate();
+  if (startTime[1] == ':' ){zeroOffsetter = '0';}
+  if (monthStr[1] == ':'){monthOffsetter='0';}
+  if (day<10){dayOffsetter = '0';}
+  return date.getFullYear().toString()+'-'+monthOffsetter+monthStr+'-'+dayOffsetter+day.toString()+' '+zeroOffsetter+startTime+':00';
+}
+
+//returns {title, summary (location), startTime, endTime}
+//params: values: same as values in getWeekly(), day: int for which day (monday is 1, tuesday is 2 etc), start/endTime in format 'TT:TT:TT'
+function eventObjMaker(values, day, startTime, endTime){
+  const current = new Date().getTime()- 18000000;
+  const lastSunday = current - (86400000 * new Date(current).getDay());//gets date for last sunday
+  var date = new Date(lastSunday + day*86400000);
+  const hold = {
+    title: values[1], 
+    summary: values[5], 
+    start: formatedDate(date, startTime), 
+    end: formatedDate(date, endTime)};
+  return hold;
+}
+//takes in [{eventID, name, startTime, endTime, dayDesignation, location, eventLead, scheduleID}]
+//returns weekly classes of [{title, summary (location), startTime, endTime}] STARTING ON THE LAST MONDAY
+function getWeekly(classes){
+  var finalClasses = [];
+  const regex = /(.*)-(.*)-(.*) (.*)/; //regex to parse startTime and endTime of elements of each course
+  for (var i = 0; i < classes.length; i++) {
+    var values = Object.values(classes[i]); // [eventID, name, startTime, endTime, dayDesignation, location, eventLead, scheduleID]
+    var dayDes = values[4];
+    // var startTimeData = [...values[2].matchAll(regex)][0]; // [original string, year, month, date, time]
+    // var endTimeData = [...values[3].matchAll(regex)][0]; // [original string, year, month, date, time]
+    if (dayDes.includes("M")){finalClasses.push(eventObjMaker(values, 1, values[2], values[3]));}
+    if(dayDes.includes("W")){finalClasses.push(eventObjMaker(values, 3, values[2], values[3]));}
+    if(dayDes.includes("F")){finalClasses.push(eventObjMaker(values, 5, values[2], values[3]));}
+    if(dayDes.includes("TH")){
+      if(dayDes.includes("TWTH") || dayDes.includes("TTH")){
+        finalClasses.push(eventObjMaker(values, 2, values[2], values[3]));
+        finalClasses.push(eventObjMaker(values, 4, values[2], values[3]));}
+      else{finalClasses.push(eventObjMaker(values, 4, values[2], values[3]));}}
+    else{
+      if(dayDes.includes("T")){finalClasses.push(eventObjMaker(values, 2, values[2], values[3]));}}
+    if(dayDes.includes("SU")){
+      if(dayDes.includes("SSU")){finalClasses.push(eventObjMaker(values, 6, values[2], values[3]));finalClasses.push(eventObjMaker(values, 7, values[2], values[3]));}
+      else{finalClasses.push(eventObjMaker(values, 7, values[2], values[3]));}}
+    else{
+      if(dayDes.includes("S")){finalClasses.push(eventObjMaker(values, 6, values[2], values[3]));}}
+  }
+  return finalClasses;
+}
 {/* HOME SCREEN FUNCTION */ }
 
 /**
@@ -132,43 +216,58 @@ function HomeScreen({ navigation }) {
   let textColor = "black"; //  default textcolor         (black in LM) (white in DM)
   const windowWidth = Dimensions.get("window").width; //full height of screen
   const windowHeight = Dimensions.get("window").height; //full width of screen
-  const [events, setEvents] = useState([
+  const courses = [
     {
       title: "CS-108",
       summary: "SB 223",
-      start: "2022-11-05 08:30:00",
-      end: "2022-11-05 09:30:00",
+      start: "2022-11-18 08:30:00",
+      end: "2022-11-18 09:30:00",
     },
     {
       title: "MATH-171",
       summary: "NH 159",
-      start: "2022-11-05 11:00:00",
-      end: "2022-11-05 11:50:00",
+      start: "2022-11-18 11:00:00",
+      end: "2022-11-18 11:50:00",
     },
     {
       title: "ENGL-101",
       summary: "CFAC 242",
-      start: "2022-11-05 12:00:00",
-      end: "2022-11-05 12:50:00",
+      start: "2022-11-18 12:00:00",
+      end: "2022-11-18 12:50:00",
     },
     {
       title: "REL-121",
       summary: "HH 133",
-      start: "2022-11-05 14:00:00",
-      end: "2022-11-05 14:50:00",
+      start: "2022-11-18 14:00:00",
+      end: "2022-11-18 14:50:00",
     },
     {
       title: "CS-195",
       summary: "SB 010",
-      start: "2022-11-03 15:00:00",
-      end: "2022-11-03 15:50:00",
+      start: "2022-11-18 15:00:00",
+      end: "2022-11-18 15:50:00",
     },
-  ]);
+  ];
+  ///testing for getWeekly()
+  // const classees = [{ eventID: 4, name: "CS-195", startTime: "2022-11-18 15:00:00", endTime: "2022-11-18 15:50:00", dayDesignation: "MTWThFSSu", location: "SB 010", eventLead: "your mom", scheduleID: 3 }];
+  // const events = getWeekly(classees);
+  //testing for getWeekly()
 
-  //On Click of event showing alert from here
+  //testing client->server
+  const student = "Braden Lint";
+  const semesterYear = "2023 Spring";
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch('https://workaroundservice.herokuapp.com/'+student+'/'+semesterYear)
+        .then((response) => response.json())
+        .then((json) => setData(json))
+        .catch((error) => console.error(error));
+  }, []);
+  //testing client->server
   const editEvent = (event) => {
     navigation.navigate('Edit')
   };
+
 
   return (
     // homeScreen view of schedule + icons
@@ -176,7 +275,7 @@ function HomeScreen({ navigation }) {
       <View style={styles.calendar}>
         <EventCalendar
           eventTapped={editEvent} // Function on event press
-          events={events} // Passing the Array of event
+          events={getWeekly(data)} // Passing the Array of event
           width={windowWidth} // Container width
           //number of dates rendered before & after initDate (def 30 before, 29 after)
           size={60} //initDate defaulting to today
@@ -198,12 +297,7 @@ function HomeStackScreen() {
       <HomeStack.Screen name="Schedule" component={HomeScreen} options={{
         headerShown: false
       }} />
-      <HomeStack.Screen name="Edit" component={EditScreen}
-        options={{
-          headerShown: false
-        }}
-      />
-      <HomeStack.Screen name="Delete" component={DeleteScreen}
+      <HomeStack.Screen name="Edit" component={EditStackScreen}
         options={{
           headerShown: false
         }}
@@ -212,9 +306,30 @@ function HomeStackScreen() {
   );
 };
 
+const EditStack = createNativeStackNavigator();
+
+function EditStackScreen() {
+  return (
+    <EditStack.Navigator>
+      <HomeStack.Screen name="Edit" component={EditScreen} options={{
+        headerShown: false
+      }} />
+      <HomeStack.Screen name="Delete" component={DeleteScreen}
+        options={{
+          headerShown: false
+        }}
+      />
+    </EditStack.Navigator>
+  );
+};
+
+
+
+
 
 function App() {
   const tabOffsetValue = useRef(new Animated.Value(0)).current;
+  const [notificationsEnabled, setNotificationsEnabled] = useState(true);
   return (
     <NavigationContainer>
       <Tab.Navigator
@@ -256,7 +371,7 @@ function App() {
               <FontAwesome5
                 name="home"
                 size={30}
-                color={focused ? 'blue' : 'gray'}
+                color={focused ? '#0167AB' : 'gray'}
               ></FontAwesome5>
             </View>
           )
@@ -265,6 +380,7 @@ function App() {
 
         {/*Settings Screen Tab*/}
         <Tab.Screen name={"Settings"} component={SettingsScreen} options={{
+          headerTitle: () => <Header />,
           tabBarIcon: ({ focused }) => (
             <View style={{
               // centring Tab Button...
@@ -274,7 +390,7 @@ function App() {
               <FontAwesome5
                 name="cog"
                 size={30}
-                color={focused ? 'blue' : 'gray'}
+                color={focused ? '#0167AB' : 'gray'}
               ></FontAwesome5>
             </View>
           )
@@ -312,13 +428,13 @@ function App() {
               <FontAwesome5
                 name="user-alt"
                 size={30}
-                color={focused ? 'blue' : 'gray'}
+                color={focused ? '#0167AB' : 'gray'}
               ></FontAwesome5>
             </View>
           )
         }} ></Tab.Screen>
 
-        <Tab.Screen name={"Notifications"} component={HomeScreen} options={{
+        <Tab.Screen name={"Notifications"} component={NotificationsScreen} options={{
           tabBarIcon: ({ focused }) => (
             <View style={{
               // centring Tab Button...
@@ -328,8 +444,16 @@ function App() {
               <FontAwesome5
                 name="bell"
                 size={30}
-                color={focused ? 'blue' : 'gray'}
-                onPress={() => alert("Notifications have been disabled")}
+                color={notificationsEnabled ? '#0167AB' : 'gray'}
+                onPress={() => {
+                  // Update the state variable when the button is pressed
+                  setNotificationsEnabled(!notificationsEnabled);
+                  if (!notificationsEnabled) {
+                    alert("Notifications have been enabled");
+                  } else {
+                    alert("Notifications have been disabled");
+                  }
+                }}
               ></FontAwesome5>
             </View>
           )
@@ -339,13 +463,6 @@ function App() {
 
   );
 }
-
-function getWidth() {
-  let width = Dimensions.get("window").width;
-  width = width - 80; // Horizontal Padding = 20...
-  return width / 5; // Total five Tabs...
-}
-
 
 
 export default App;
